7495d3d43def29939fc5bccd5d81affc1945a725
checkiamsiam <issiam02415@gmail.com>
2025-02-24 23:58:50 +0600

    feat: enhance NewsroomSection with carousel functionality and update dummy image; improve heading styles

--- src/app/(main)/_components/newsroom-section.tsx
+++ src/app/(main)/_components/newsroom-section.tsx
@@ -1,7 +1,9 @@
+"use client";
 import { NextMoveLink } from "@/components/ui/next-move-link";
 import { NewsCard } from "./news-card";
+import useEmblaCarousel from "embla-carousel-react";
 
-const dummyImage = "/placeholder.svg";
+const dummyImage = "https://www.vestaboard.com/hubfs/LA%20FIRES%2016x9.png";
 
 const newsItems = [
   {
@@ -37,18 +39,31 @@ const newsItems = [
 ];
 
 export function NewsroomSection() {
+   const [emblaRef] = useEmblaCarousel({ loop: false });
   return (
     <section className="bg-primary py-16 md:py-24">
       <div className="container px-4 md:px-6">
-        <div className="mb-12 sm:flex items-center justify-between">
-          <h2 className="text-5xl font-normal text-foreground max-sm:mb-5">
+        <div className="mb-12 flex items-center justify-between">
+          <h2 className="sm:text-5xl text-2xl sm:font-normal font-bold text-foreground max-sm:mb-5">
             Newsroom
           </h2>
-          <div>
+          <div className="max-sm:-mt-5">
             <NextMoveLink href="#">View all news</NextMoveLink>
           </div>
         </div>
-        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+        <div className="overflow-hidden lg:hidden" ref={emblaRef}>
+          <div className="flex">
+            {newsItems.map((item, index) => (
+              <div
+                key={index}
+                className="flex-[0_0_100%] min-w-0 pl-4 first:pl-0"
+              >
+                <NewsCard {...item} />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 max-lg:hidden">
           {newsItems.map((item, index) => (
             <NewsCard key={index} {...item} />
           ))}
