84b2d4d08abd605c076450d5f31794593125754c
itsproali <itsproali@gmail.com>
2025-02-26 21:39:09 +0600

    feat: enhance customer section with internationalization support and dynamic client data

--- src/app/[locale]/_components/comparison-section.tsx
+++ src/app/[locale]/_components/comparison-section.tsx
@@ -1,11 +1,14 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
 import { Card } from "@/components/ui/card";
 import { NextMoveLink } from "@/components/ui/next-move-link";
 import { Apple, Globe, Smartphone } from "lucide-react";
+import { useTranslations } from "next-intl";
 import { useEffect, useState } from "react";
 
 export default function ComparisonSection() {
+  const t = useTranslations("section_6");
   const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
@@ -17,65 +20,35 @@ export default function ComparisonSection() {
         <div className="max-w-6xl mx-auto w-full">
           {/* Heading Section */}
           <div className="text-center mb-16 max-sm:px-5">
-            <h1 className="sm:text-4xl md:text-5xl text-3xl md:font-light font-bold text-foreground mb-6">
-              The best of analog and digital
-            </h1>
-            <p className="text-lg md:text-2xl text-foreground max-w-3xl mx-auto font-light">
-              It&apos;s not a digital screen. 8,448 magical spinning flaps you
-              control with your device from anywhere.
-            </p>
+            <h1 className="sm:text-4xl md:text-5xl text-3xl md:font-light font-bold text-foreground mb-6">{t("title")}</h1>
+            <p className="text-lg md:text-2xl text-foreground max-w-3xl mx-auto font-light">{t("description")}</p>
           </div>
 
           {/* Cards Section */}
           <div className="max-w-6xl mx-auto flex sm:flex-row flex-col justify-center">
             {/* Left Card */}
             <Card className="z-10 bg-secondary p-8 sm:p-12 sm:pr-28 py-20 sm:max-w-md max-sm:mx-auto max-sm:rounded-none">
-              <h2 className="text-3xl md:text-4xl font-light mb-4">
-                Controlled from your phone or desktop
-              </h2>
-              <p className="text-background mb-8 font-light">
-                Inspire others, display lists, menus, quotes, patterns, or
-                connect to hundreds of services for automated content*.
-              </p>
+              <h2 className="text-3xl md:text-4xl font-light mb-4">{t("feature_1.title")}</h2>
+              <p className="text-background mb-8 font-light">{t("feature_1.description")}</p>
               <div className="flex items-center gap-6 mb-8">
                 <Apple className="w-6 h-6" />
                 <Smartphone className="w-6 h-6" />
                 <Globe className="w-6 h-6" />
               </div>
-              <NextMoveLink
-                href="#"
-                className="text-background"
-                textClassName="border-background"
-              >
+              <NextMoveLink href="#" className="text-background" textClassName="border-background">
                 Learn More
               </NextMoveLink>
 
-              <p className="text-xs text-grey-darker mt-8">
-                *integrations and more now available with Vistaboard+
-              </p>
+              <p className="text-xs text-grey-darker mt-8">*integrations and more now available with Vistaboard+</p>
             </Card>
 
             {/* Right Video Card */}
             <Card className="sm:max-w-sm sm:my-5 border-none rounded-2xl overflow-hidden sm:-ml-20 max-sm:mx-auto max-sm:w-full max-sm:h-auto z-30 max-sm:-mt-5">
               {!isClient ? (
-                <img
-                  src="/placeholder.svg"
-                  alt="Loading..."
-                  className="h-full w-full object-cover"
-                />
+                <img src="/placeholder.svg" alt="Loading..." className="h-full w-full object-cover" />
               ) : (
-                <video
-                  autoPlay
-                  muted
-                  loop
-                  playsInline
-                  className="h-full w-full object-cover"
-                  poster="/placeholder.svg"
-                >
-                  <source
-                    src="https://1984131.fs1.hubspotusercontent-na1.net/hubfs/1984131/Test_ThumbnailVideo.mp4"
-                    type="video/mp4"
-                  />
+                <video autoPlay muted loop playsInline className="h-full w-full object-cover" poster="/placeholder.svg">
+                  <source src="https://1984131.fs1.hubspotusercontent-na1.net/hubfs/1984131/Test_ThumbnailVideo.mp4" type="video/mp4" />
                 </video>
               )}
             </Card>
@@ -88,28 +61,15 @@ export default function ComparisonSection() {
           <div className="max-w-6xl mx-auto flex sm:flex-row flex-col-reverse justify-center items-center">
             {/* Left Card */}
             <div className="sm:-mr-40 max-sm:-mt-12  max-sm:mx-auto  z-30">
-              <img
-                src="https://www.vestaboard.com/hubfs/vb-bit-persp.png"
-                alt="Loading..."
-                className="h-full w-full object-cover"
-              />
+              <img src="https://www.vestaboard.com/hubfs/vb-bit-persp.png" alt="Loading..." className="h-full w-full object-cover" />
             </div>
 
             {/* Right Video Card */}
             <Card className="z-10 bg-secondary p-8 sm:p-12 sm:py-32 sm:pl-20 py-20 max-w-md max-sm:mx-auto max-sm:rounded-none">
-              <h2 className="text-2xl md:text-4xl font-light mb-4">
-                Award-winning product design
-              </h2>
-              <p className="text-grey-darker mb-5 font-light">
-                Built with the highest quality, vetted materials. Designed by a
-                seasoned team of experts, it&apos;s beautiful inside and out.
-              </p>
+              <h2 className="text-2xl md:text-4xl font-light mb-4">{t("feature_2.title")}</h2>
+              <p className="text-grey-darker mb-5 font-light">{t("feature_2.description")}</p>
 
-              <NextMoveLink
-                href="#"
-                className="text-background"
-                textClassName="border-background"
-              >
+              <NextMoveLink href="#" className="text-background" textClassName="border-background">
                 Learn More
               </NextMoveLink>
             </Card>
--- src/app/[locale]/_components/customers-section.tsx
+++ src/app/[locale]/_components/customers-section.tsx
@@ -3,10 +3,40 @@
 import { Button } from "@/components/ui/button";
 import useEmblaCarousel from "embla-carousel-react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 import { useCallback } from "react";
 
 export default function CustomerSection() {
+  const t = useTranslations("section_7");
+
+  const clients = [
+    {
+      name: t("client_1.name"),
+      image: "https://www.vestaboard.com/hubfs/image-1%20square.png",
+      challenge: t("client_1.challenge"),
+      solution: t("client_1.solution"),
+    },
+    {
+      name: t("client_2.name"),
+      image: "https://www.vestaboard.com/hubfs/image-1%20square.png",
+      challenge: t("client_2.challenge"),
+      solution: t("client_2.solution"),
+    },
+    {
+      name: t("client_3.name"),
+      image: "https://www.vestaboard.com/hubfs/image-1%20square.png",
+      challenge: t("client_3.challenge"),
+      solution: t("client_3.solution"),
+    },
+    {
+      name: t("client_4.name"),
+      image: "https://www.vestaboard.com/hubfs/image-1%20square.png",
+      challenge: t("client_4.challenge"),
+      solution: t("client_4.solution"),
+    },
+  ];
+
   const [emblaRef, emblaApi] = useEmblaCarousel({
     align: "start",
     loop: true,
@@ -20,14 +50,13 @@ export default function CustomerSection() {
   const scrollNext = useCallback(() => {
     if (emblaApi) emblaApi.scrollNext();
   }, [emblaApi]);
+
   return (
     <div className="bg-foreground py-20">
       <div className="container mx-auto mb-20 -mt-96">
         <div className="flex md:flex-row flex-col items-center justify-between gap-5">
-          <h2 className="sm:text-4xl text-3xl md:text-6xl text-foreground  lg:font-light font-bold max-md:w-full max-md:text-center">
-            Meet Our {" "}
-            <br className="max-md:hidden"/>
-            Customers
+          <h2 className="sm:text-4xl text-3xl md:text-6xl text-foreground lg:font-light font-bold max-md:w-full max-md:text-center max-w-lg">
+            {t("title")}
           </h2>
           <Button
             variant="outline"
@@ -38,47 +67,28 @@ export default function CustomerSection() {
         </div>
       </div>
       <div className="relative  min-h-[60vh]">
-        <div
-          className="overflow-hidden sm:ml-[max(1rem,calc((100%-85rem)/2))]"
-          ref={emblaRef}
-        >
+        <div className="overflow-hidden sm:ml-[max(1rem,calc((100%-85rem)/2))]" ref={emblaRef}>
           <div className="flex gap-6 ">
-            {[1, 2, 4, 5, 6, 7, 8].map((customer, i) => (
+            {clients?.map((customer, i) => (
               <div
                 key={i}
                 className={`bg-transparent border-0 pl-5  lg:flex-[0_0_380px] sm:flex-[0_0_210px] flex-[0_0_95%] justify-center items-center `}
               >
                 <div className="relative aspect-[4/4] mb-4">
-                  <Image
-                    src="https://www.vestaboard.com/hubfs/image-1%20square.png"
-                    alt="placeholder"
-                    fill
-                    className="object-cover rounded-lg"
-                  />
+                  <Image src={customer?.image} alt="placeholder" fill className="object-cover rounded-lg" />
                 </div>
-                <h3 className="text-background font-bold mb-2">
-                  Wall Street Journal enhances live event experience with
-                  Vestaboard
-                </h3>
-                <div className="flex gap-2 flex-wrap text-xs">
-                  Customer Stories, Home, Vestaboard White
-                </div>
+                <h3 className="text-background font-bold mb-2">{customer.challenge}</h3>
+                <div className="flex gap-2 flex-wrap text-xs">{customer.solution}</div>
               </div>
             ))}
           </div>
         </div>
 
         <div className="container mx-auto flex justify-between items-center sm:px-10 absolute lg:top-1/3 sm:top-[15%] top-[30%] sm:ml-[max(1rem,calc((100%-85rem)/2))]">
-          <button
-            className=" bg-black/60 backdrop-blur-sm rounded-full p-2 flex justify-center items-center"
-            onClick={scrollPrev}
-          >
+          <button className=" bg-black/60 backdrop-blur-sm rounded-full p-2 flex justify-center items-center" onClick={scrollPrev}>
             <ChevronLeft className="size-8 text-foreground" />
           </button>
-          <button
-            className=" bg-black/60 backdrop-blur-sm rounded-full p-2 flex justify-center items-center"
-            onClick={scrollNext}
-          >
+          <button className=" bg-black/60 backdrop-blur-sm rounded-full p-2 flex justify-center items-center" onClick={scrollNext}>
             <ChevronRight className="size-8 text-foreground" />
           </button>
         </div>
