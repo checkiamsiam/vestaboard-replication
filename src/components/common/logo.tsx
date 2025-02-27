fcc82efa9848ebf23f66489616f82fb9195dcaf9
itsproali <itsproali@gmail.com>
2025-02-28 00:12:12 +0600

    fix: update logo link to point to the homepage

--- src/components/common/logo.tsx
+++ src/components/common/logo.tsx
@@ -2,7 +2,7 @@ import { Link } from "@/i18n/routing";
 
 export function Logo() {
   return (
-    <Link href="#">
+    <Link href="/">
       <div className="w-32">
         <img src="/images/logo.png" alt="Vestaboard" />
       </div>
